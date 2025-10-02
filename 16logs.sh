67bdb899fc0e969e63603f65676b7a455eb96d83
SANDEEP316244 <sandeep.adari007@gmail.com>
2025-10-02 19:20:28 +0530

    Update 16logs.sh

--- 16logs.sh
+++ 16logs.sh
@@ -3,19 +3,29 @@ user_id=$(id -u)
 
 logs_folder="/var/log/shell-script"
 script_name=$(echo $0 | cut -d "." -f1 )
-log_file="$log_folder/$script_name.log"
-mkdir $logs_folder
+log_file="$logs_folder/$script_name.log"
+mkdir -p $logs_folder
 echo " script started executing $(date) " | tee -a $log_file
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $? -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $log_file
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $log_file
+    fi
+}
 
 
 if [ $user_id -ne 0 ]; then
 echo " RUN AS A SUDOER "
-fi 
+fi
 
-dnf list installed mysql 
+dnf list installed mysql &>>$log_file
 
 if [ $? -ne 0 ]; then
-dnf install mysql
+dnf install mysql &>>$log_file
+VALIDATE "MySQL"
 else
 echo " mysql already installed "
 fi
