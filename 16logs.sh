78cea3a4577d2a4a87816b1cabab1653d98c6072
SANDEEP316244 <sandeep.adari007@gmail.com>
2025-10-02 18:51:07 +0530

    Update 16logs.sh

--- 16logs.sh
+++ 16logs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 user_id=$(id -u)
 
-
+logs_folder="/var/log/shell-script"
+script_name=$(echo $0 | cut -d "." -f1 )
+log_file="$log_folder/$script_name.log"
+mkdir $logs_folder
+echo " script started executing $(date) " | tee -a $log_file
 
 
 if [ $user_id -ne o ]; then
